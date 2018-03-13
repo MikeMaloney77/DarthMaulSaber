3eb5b1c96817985e453a8a5d556e66c1ee630a5e
MikeMaloney77 <36928523+MikeMaloney77@users.noreply.github.com>
2018-03-13 18:52:31 -0500

    Update TestFile.py

--- TestFile.py
+++ TestFile.py
@@ -1,13 +1,24 @@
-from LightSaberHandle import *
 from DisplayCase import *
+from graphics import *
+
 
 def main():
     """Runs the main code"""
 
-    handle1 = Handle(100, win)
-    handle2 = Handle(100, win, 160)
+    DisplayCase(win)
+    handle1 = Handle(100, win, 160)
+    handle1.drawBlade(win)
+    time.sleep(.25)
+    handle1.drawButton(win, 415, 175)
+    handle1.changeBladeColor(win)
+    handle2 = Handle(100, win)
+    handle2.drawBlade(win)
+    handle2.drawButton(win, 415, 325)
+    handle2.changeBladeColor(win)
     handle3 = Handle(100, win, 460)
-    case = DisplayCase(win)
+    handle3.drawBlade(win)
+    handle3.drawButton(win, 415, 475)
+    handle3.changeBladeColor(win)
 
 
     win.getMouse()
