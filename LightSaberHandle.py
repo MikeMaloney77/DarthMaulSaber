9a4a641688d58830aac2c5da786fc9d6d4241de9
MikeMaloney77 <36928523+MikeMaloney77@users.noreply.github.com>
2018-03-15 18:37:43 -0500

    Update LightSaberHandle.py

--- LightSaberHandle.py
+++ LightSaberHandle.py
@@ -1,22 +1,23 @@
 from Blade import *
 
 class Handle(Blade):
-    def __init__(self, x, win, y = 310):
-        super(Handle, self).__init__(x, win, y, color = "red", width = 298, height = 30)
-        self.drawHandle("gray", win)
+    def __init__(self, x, win, y = 310, color2 = "gray"):
+        super(Handle, self).__init__(x, y)
+        self.color2 = color2
+        self.drawHandle(win)
         self.drawLine(win)
-        self.buttonPress(win)
+        # self.buttonPress(win)
 
 
-    def drawHandle(self, color, win):
+    def drawHandle(self, win):
         """Draws the lightsaber handle"""
         hand = Rectangle(Point(self.x + 298, self.y), Point(self.x + 502, self.y + 30))
-        hand.setFill(color)
+        hand.setFill(self.color2)
         hand.draw(win)
 
 
     def drawLine(self, win, newX = 432):
-        """Draws stripes on the handle"""
+        """Draws lines on the handle"""
         for line in range(6):
             line = Line(Point(newX, self.y), Point(newX, self.y + self.height))
             line.setWidth(2)
@@ -35,9 +36,9 @@ class Handle(Blade):
             button.setFill(self.color)
             button.setActiveFill("white")
 
-    def buttonPress(self, userInput,win):
-        """This will make the lightsaber change color when you press the button"""
-        user = if userInput == "b":
-                self.changeBladeColor.setFill("blue")
-        user.draw(win)
-        
+
+    # def buttonPress(self, userInput, win):
+    #    """This will make the lightsaber change color when you press the button"""
+    #   user = if userInput == "b":
+    #     self.changeBladeColor.setFill("blue")
+    #     user.draw(win)
